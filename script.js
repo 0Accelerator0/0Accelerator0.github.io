391c145f23026cc710b53d393528e997dc083f7e
0Accelerator0 <0Accelerator0@users.noreply.github.com>
2016-06-16 08:42:38 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,51 @@ var main = function(){
         $('.dropdown-menu').hide(300);
     });
     
+    carousel();
 }
+
+function carousel() {
+    slidenext();
+    setTimeout(carousel, 5000); // Change image every 5 seconds
+}
+
+var slidenext = function(){
+    var currentSlide = $('.active-slide');
+    var nextSlide = currentSlide.next();
+
+    var currentDot = $('.active-dot');
+    var nextDot = currentDot.next();
+
+    if(nextSlide.length === 0) {
+      nextSlide = $('.slide').first();
+      nextDot = $('.dot').first();
+    }
+    
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    nextSlide.fadeIn(600).addClass('active-slide');
+
+    currentDot.removeClass('active-dot');
+    nextDot.addClass('active-dot');
+};
+
+var slideprev = function(){
+    var currentSlide = $('.active-slide');
+    var prevSlide = currentSlide.prev();
+
+    var currentDot = $('.active-dot');
+    var prevDot = currentDot.prev();
+
+    if(prevSlide.length === 0) {
+      prevSlide = $('.slide').last();
+      prevDot = $('.dot').last();
+    }
+    
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    prevSlide.fadeIn(600).addClass('active-slide');
+
+    currentDot.removeClass('active-dot');
+    prevDot.addClass('active-dot');
+};
 
 
 
